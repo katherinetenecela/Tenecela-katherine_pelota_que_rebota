a7580a8c7baffaba875c4dd4092208f3ecd21e64
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-09-28 16:50:53 -0500

    pelota rebota

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+let posX, posY, diam, rad;
+let esp = 34;
+let margen = 30;
+let piso;
+let velY = 5;
+let gravity = 0.2;
+let friction = 0.5;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  posX = windowWidth / 2;
+  posY = 100;
+  rectMode(CENTER);
+  esp = 34;
+  piso = windowHeight - margen - esp / 2;
+  frameRate(10);
+  diam = 50;
+  rad = diam / 2;
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(0, 200, 150, 70);
+  noStroke();
+
+  posY += velY;
+
+  if (posY > piso - rad) {
+    velY *= -1;
+  }
+  fill(255);
+  circle(posX, posY, diam);
+
+  fill(255, 0, 0);
+  rect(windowWidth * 0.5, windowHeight - margen, windowWidth * 0.8, esp);
 }
